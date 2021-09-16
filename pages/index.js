9e97c05f00d836b7b17678767516791a91f7b57d
caosutang <FS124937@firstsolar.com>
2021-09-16 13:59:42 +0700

    Detail product page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
-const Home = () => {
-  return <div>Home</div>;
+import { useState } from "react";
+import Head from "next/head";
+import { getData } from "../utils/fetchData";
+import ProductItem from "../components/product/ProductItem";
+
+const Home = ({ productProps }) => {
+  const [products, setProducts] = useState(productProps);
+  return (
+    <div className="products">
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      {products.length === 0 ? (
+        <h2>No products</h2>
+      ) : (
+        products.map((product) => (
+          <ProductItem key={product._id} product={product}></ProductItem>
+        ))
+      )}
+    </div>
+  );
 };
+
+export async function getServerSideProps() {
+  const res = await getData("product");
+  return {
+    props: {
+      productProps: res.products,
+      result: res.result,
+    },
+  };
+}
 
 export default Home;
---
+++ pages/product/[id].js
@@ -0,0 +1,91 @@
+import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
+import { getData } from "../../utils/fetchData";
+
+const DetailProduct = (props) => {
+  const [product] = useState(props.product);
+  const [tab, setTab] = useState(0);
+  const imgRef = useRef();
+
+  const isActive = (index) => {
+    if (tab === index) return " active";
+    return "";
+  };
+
+  // useEffect(() => {
+  //   const images = imgRef.current.children;
+  //   for (let i = 0; i < images.length; i++) {
+  //     images[i].className = images[i].className.replace(
+  //       "active",
+  //       "img-thumbnail rounded"
+  //     );
+  //   }
+  //   images[tab].className = "img-thumbnail rounded active";
+  // }, [tab]);
+
+  return (
+    <div className="row detail-page">
+      <Head>
+        <title> Detail Product</title>
+      </Head>
+      <div className="col-md-6">
+        <img
+          src={product.images[tab].url}
+          alt={product.images[tab].url}
+          className="d-block img-thumbnail rounded mt-4 w-100"
+          style={{ height: "350px" }}
+        />
+        <div
+          className="row mx-0"
+          style={{ cursor: "pointer" }}
+          // ref={imgRef}
+        >
+          {product.images.map((img, index) => {
+            return (
+              <img
+                src={img.url}
+                alt={img.url}
+                key={index}
+                className={`img-thumbnail rounded ${isActive(index)}`}
+                style={{ height: "80px", width: "20%" }}
+                onClick={() => setTab(index)}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className="col-md-6 mt-3">
+        <h2 className="text-uppercase">{product.title}</h2>
+        <h5 className="text-danger">${product.price}</h5>
+        <div className="row mx-0 d-flex justify-content-between">
+          {product.inStock > 0 ? (
+            <h6 className="text-danger">In Stock</h6>
+          ) : (
+            <h6 className="text-danger">Out of Stock</h6>
+          )}
+          <h6 className="text-danger">Sold: {product.sold}</h6>
+        </div>
+        <div className="my-2">{product.description}</div>
+        <div className="my-2">
+          {product.content}
+          {product.content}
+          {product.content}
+        </div>
+        <button type="button" className="btn btn-dark d-block my-3 px-5">
+          Buy
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export async function getServerSideProps({ params: { id } }) {
+  const res = await getData(`product/${id}`);
+  return {
+    props: {
+      product: res.product,
+    },
+  };
+}
+
+export default DetailProduct;
